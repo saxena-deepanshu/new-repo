c06fa61c44f674812819f998cc19b32f4fd647c1
Rishabh Jain <91656132+Rishabh2004@users.noreply.github.com>
2022-05-21 14:49:37 +0530

    Update and rename new file.c to add.c

---
+++ add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<conio.h>
+main()
+{
+  int a,b;
+  printf(" enter a number");
+  scanf("%d",&a):
+  printf("\n enter another number for addition");
+  printf("sum of %d and %d is %d",a,b,c);
+  getch():
+}
